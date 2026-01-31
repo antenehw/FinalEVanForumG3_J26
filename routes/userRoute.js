97db161cb19910801e301e0938e78ea605ebf9f2
Anteneh <antetaye@yahoo.com>
2026-01-31 15:48:17 -0600

    db created on Railway, updated the link and deployed

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,21 +1,27 @@
+
 const express = require("express");
 const router = express.Router();
 
-//import authentication middleware
-const authMiddleware = require("../middleware/authMiddleware")
 
 //user controllers
 const {register,login,checkUser} = require("../controller/userController")
 
-//register route
+//import authentication middleware
+const authMiddleware = require("../middleware/authMiddleware")
+
+
+// Routes
+
 router.post("/register", register)
-
-
-//login routes
 router.post("/login", login);
-
-//check users
-router.get("/check", authMiddleware,checkUser);
+router.get("/check", authMiddleware, checkUser);
 
 
-module.exports = router
+module.exports = router;
+
+
+
+
+
+
+
