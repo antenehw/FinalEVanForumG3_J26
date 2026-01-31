83b081106ed2fc088dd2b84d0621ba9142431a6c
Anteneh <antetaye@yahoo.com>
2026-01-31 15:45:09 -0600

    db created on Railway, updated the link and deployed

--- client/src/axiosconfig.js
+++ client/src/axiosconfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("Loaded API URL:", import.meta.env.VITE_API_URL);
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://finalevanforumg3j26-production.up.railway.app/api",
 });
 
 instance.interceptors.request.use(
